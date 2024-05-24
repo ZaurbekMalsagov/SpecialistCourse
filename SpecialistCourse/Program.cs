7fad0fa720156b3724b04174b6a54ebfe02c4f48
Zaurbek Malsagov <lordmalsagov@gmail.com>
2024-05-24 14:57:29 +0300

    add day 1 labs

--- SpecialistCourse/Program.cs
+++ SpecialistCourse/Program.cs
@@ -2,9 +2,35 @@
 {
     internal class Program
     {
+        enum Figures { Triangle, Rectangle, Circle}
+
+        struct FiguresData
+        {
+            public int x, y;
+            public string colour;
+            public Figures ftype;
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Specialist Course!");
+            // MOD1
+            // Console.WriteLine("Hello, Specialist Course!");
+
+            // MOD2
+            FiguresData figuresData = new FiguresData();
+            figuresData.ftype = Figures.Rectangle;
+            figuresData.x = 23;
+            figuresData.y = figuresData.x;
+            figuresData.colour = "red";
+
+            Console.WriteLine($"Figures type = {figuresData.ftype};\n" +
+                              $"Figures colour = {figuresData.colour};\n" +
+                              $"Figures coordinate = ({figuresData.x},{figuresData.y})");
+
+            Console.ReadLine();
+
         }
+
     }
+    
 }
