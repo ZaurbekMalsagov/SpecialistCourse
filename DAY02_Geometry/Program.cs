eec962a78af7e4a8fb586e66732cfacf518d13fc
Zaurbek Malsagov <lordmalsagov@gmail.com>
2024-05-28 11:50:49 +0300

    add geometry project

---
+++ DAY02_Geometry/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Geometry
+{
+    enum Figures { Romb, Rect, RightTriangle, Circle }
+    struct Fdata
+    {
+        public int x0, y0;
+        public double a, b;
+        public int color;
+        public Figures type;
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Fdata fd = new Fdata() { a = 4, b = 3, type = Figures.Romb };
+            //Console.WriteLine(area(fd));
+            Console.WriteLine("Ok");
+        }
+
+        private static double CalculateSquare(Fdata fd)
+        {
+            double square = 0;
+            switch (fd.type)
+            {
+                case Figures.Romb:
+                    square = 0.5 * fd.a *fd.b;
+                    break;
+                case Figures.Rect:
+                    square = fd.a * fd.b;
+                    break;
+                case Figures.RightTriangle:
+                    square = (fd.a * fd.b)/2;
+                    break;
+                case Figures.Circle:
+                    square = Math.PI * Math.Pow(fd.a,2);
+                    break;
+            }
+
+            return square;
+        }
+    }
+}
