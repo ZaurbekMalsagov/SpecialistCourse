3fe796ea19e81d4eb685a3704909677c26b81393
ZaurbekMalsagov <lordmalsagov@gmail.com>
2024-05-30 17:38:06 +0300

    add class shape

---
+++ DAY03/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY03
+{
+    public abstract class Shape
+    {
+        private string Colour {  get; set; }
+        private int X {  get; set; }
+        private int Y { get; set; }
+
+        public Shape(int x, int y, string colour = "BLACK") { 
+            this.X = x; 
+            this.Y = y; 
+            Colour = colour;
+        }
+
+        public abstract void Draw();
+    }
+}
