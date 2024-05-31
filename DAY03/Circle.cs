b1064915ca3a3754667776e990da70871824925b
ZaurbekMalsagov <lordmalsagov@gmail.com>
2024-05-31 11:51:44 +0300

    add class Circle

---
+++ DAY03/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY03
+{
+    internal class Circle : Shape
+    {
+        private int X { get; set; }
+        private int Y { get; set; }
+        private double Scale { get; set; }
+        private double Radius {  get; set; }
+
+        public Circle(int x, int y, double radius, int scale = 1, string colour = "Black")
+        {
+            this.X = x;
+            this.Y = y;
+            this.Radius = radius * scale;
+            this.Scale = scale;
+            this.Colour = colour;
+
+        }
+
+        public override void Draw() =>  Console.WriteLine($"This circle point ({X},{Y}), radius = {Radius}, colour = {Colour} and Scale = {Scale}");
+
+        public override string ToString() => $"This circle point ({X},{Y}), radius = {Radius}, colour = {Colour} and Scale = {Scale}";
+
+        public void MoveBy(int dx, int dy)
+        {
+            this.X += dx;
+            this.Y += dy;
+        }
+
+        public void AddScale(double factor)
+        {
+            Scale = factor;
+            this.Radius *= factor;
+        }
+    }
+}
--- DAY03/Program.cs
+++ DAY03/Program.cs
@@ -9,6 +9,12 @@
             p1.MoveBy(1, 2);
             Console.WriteLine(p1.ToString());
 
+            Circle c1 = new Circle(1, 1, 5);
+            c1.Draw();
+            c1.MoveBy(1, 2);
+            c1.AddScale(3);
+            Console.WriteLine(c1.ToString());
+
         }
     }
 }
