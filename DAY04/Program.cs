b2aedc076896aa16f8e961df1b8b4f3e9de64a79
ZaurbekMalsagov <lordmalsagov@gmail.com>
2024-05-31 17:43:34 +0300

    add task with reflection and garbage collector

---
+++ DAY04/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+
+namespace DAY04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateAndKillClass();
+            // Type? stringType = Type.GetType("System.String");
+            Type? stringInfo = typeof(string).GetTypeInfo();
+            foreach (var typeInfo in stringInfo.GetMethods())
+            {
+                Console.WriteLine(typeInfo.Name);
+            }
+            Console.WriteLine(stringInfo.Name);
+            
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+        }
+
+        static void CreateAndKillClass()
+        {
+            DeadClass dead = new DeadClass();
+        }
+
+       
+        
+    }
+
+    class DeadClass
+    {
+        public DeadClass()
+        {
+            Console.WriteLine("Я родился");
+        }
+
+        ~DeadClass()
+        {
+            Console.WriteLine("Меня дезинтегрировали");
+        }
+    }
+}
